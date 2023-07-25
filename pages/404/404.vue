39132857d9edf58789b3e8aa52056722eb9e686a
XCSenior <2969271422@qq.com>
2023-07-25 12:41:39 +0800

    Initial commit

---
+++ pages/404/404.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<h1>当前页面不存在</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
